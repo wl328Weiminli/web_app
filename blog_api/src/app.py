94ab0e19fe87ef7d3394207af992960547aaa7ed
wl328Weiminli <wl328@njit.edu>
2019-12-16 21:01:09 -0500

    Feature: set the database model

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 from .config import app_config
 
+from .models import db, bcrypt
+
 
 def create_app(env_name):
     """
@@ -14,6 +16,10 @@ def create_app(env_name):
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+
+    bcrypt.init_app(app)  # add this line
+
+    db.init_app(app)  # add this line
 
     @app.route('/', methods=['GET'])
     def index():
